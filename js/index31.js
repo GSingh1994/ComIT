c3f66eb681b8c6d89c8561bcdefb47b961a3545d
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-05 02:25:09 -0500

    Added Exercise 31

---
+++ js/index31.js
@@ -0,0 +1,8 @@
+let firstSide = 10;
+let secondSide = 20;
+let thirdSide = 5;
+const perimeterOfTriangle = firstSide + secondSide + thirdSide;
+
+const result = `The perimeter of triangle with side length of ${firstSide}, ${secondSide} and ${thirdSide} is ${perimeterOfTriangle}`;
+
+console.log(result);
