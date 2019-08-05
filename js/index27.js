c448a4b0e217401f61146449989aa855328251ef
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-05 02:04:10 -0500

    Added Exercise 27

---
+++ js/index27.js
@@ -0,0 +1,14 @@
+let firstNumber = 2;
+let secondNumber = 3;
+
+let add = firstNumber + secondNumber;
+let substract = firstNumber - secondNumber;
+let multiply = firstNumber * secondNumber;
+let divide = secondNumber / firstNumber;
+let remainder = firstNumber % secondNumber;
+
+console.log(`I add 2 + 3 and the result is ${add}`);
+console.log(`I substract 2 - 3 and the result is ${substract}`);
+console.log(`I multiply 2 * 3 and the result is ${multiply}`);
+console.log(`I divide 3 / 2 and the result is ${divide}`);
+console.log(`The remainder of 2 % 3 is ${remainder}`);
