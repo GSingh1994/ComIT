25bfd496d0c2c5cd0ac9ae308fa69d74986f160c
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-05 01:47:49 -0500

    Added Exercise 26

---
+++ js/index26.js
@@ -0,0 +1,20 @@
+let cityName = 'Winnipeg';
+let isBigCity = false;
+let traffic = true;
+let airport = 1;
+let mayorName = undefined;
+let isBeautiful = null;
+
+console.log('cityName', cityName);
+console.log('isBigCity', isBigCity);
+console.log('traffic', traffic);
+console.log('airport', airport);
+console.log('mayorName', mayorName);
+console.log('isBeautiful', isBeautiful);
+
+console.log('cityName', typeof cityName);
+console.log('isBigCity', typeof isBigCity);
+console.log('traffic', typeof traffic);
+console.log('airport', typeof airport);
+console.log('mayorName', typeof mayorName);
+console.log('isBeautiful', typeof isBeautiful);
