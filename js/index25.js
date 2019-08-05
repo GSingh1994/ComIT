4c302589aaca684b657ee1673c6b612ff7ddac6c
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-05 01:37:29 -0500

    Added Exercise 25

---
+++ js/index25.js
@@ -0,0 +1,13 @@
+let superHeroName = 'Batman';
+let name = 'Bruce Wayne';
+let fly = false;
+let hasBatmobile = true;
+let life = 63;
+let freeTime = null;
+
+console.log('superHeroName', superHeroName);
+console.log('name', name);
+console.log('fly', fly);
+console.log('hasBatmobile'), hasBatmobile;
+console.log('life', life);
+console.log('freeTime', freeTime);
