2449e1571fd0ad43f6eff9a7c9431e52fde578c3
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-05 00:54:30 -0500

    Added Exercise 21

---
+++ js/index21.js
@@ -0,0 +1,8 @@
+let age = 24;
+let phone = 2048940345;
+let streetName = "St. Anne's";
+let streetNumber = 267;
+
+console.log(`I'm ${age} years old`);
+console.log(`My phone number is: ${phone}`);
+console.log(`I live on ${streetNumber} ${streetName}`);
