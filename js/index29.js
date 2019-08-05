ff8fa52b1d12750d78fe3e5bd36cfba276767edb
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-05 02:16:20 -0500

    Added Exercise 29

---
+++ js/index29.js
@@ -0,0 +1,6 @@
+let sideLengthOfSquare = 10;
+let perimeterOfSquare = 4 * sideLengthOfSquare;
+
+let result = `The perimeter of square with side length of ${sideLengthOfSquare} is ${perimeterOfSquare}`
+
+console.log(result);
