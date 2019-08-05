8ab20e60224993a8c07a0767343591c112a3378a
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-04 20:40:24 -0500

    Added Exercise 5

---
+++ js/index5.js
@@ -0,0 +1,11 @@
+firstName = 'Gagandeep';
+lastName = 'singh';
+age = 24;
+dateOfBirth = '05/16/1994';
+address = '267 St. Annes road';
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(dateOfBirth);
+console.log(address);
