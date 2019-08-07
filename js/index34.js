c08ce39af71bc449b8ca0a78c02489e8ba4ecc2e
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-07 01:28:20 -0500

    added Exercise 34

---
+++ js/index34.js
@@ -0,0 +1,9 @@
+let income, revenue, taxes;
+
+income = 1000;
+revenue = 600;
+taxes = 500;
+
+console.log(`Income Objective: ${income >= 800} 
+Taxes objective: ${taxes < 400}
+Bonus objective: ${revenue == 600}`)
