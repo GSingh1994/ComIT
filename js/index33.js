913f23f50f5662db35fb6a3d177837168987321d
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-07 01:15:04 -0500

    added Exercise 33

---
+++ js/index33.js
@@ -0,0 +1,18 @@
+let userCount = 100;
+userCount += 5;
+
+console.log('Users count:', userCount)
+
+userCount -= 3;
+
+console.log('Users count:', userCount)
+
+// brought new company
+userCount *= 2;
+
+console.log('Users count:', userCount)
+
+let menCount = userCount / 2;
+let womenCount = userCount / 2;
+
+console.log(`Users count: ${menCount} & ${womenCount}`)
