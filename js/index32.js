b5097e38ad4914fd29863461b9c4db1752ca6804
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-06 21:14:20 -0500

    added Exercise 32

---
+++ js/index32.js
@@ -0,0 +1,10 @@
+let height = 10;
+let base = 4;
+const perimeterOfRectangle = 2 * (base + height);
+
+const areaOfRectangle = base * height;
+
+
+console.log(`The perimeter of rectangle with base of ${base} and height of ${height} is ${perimeterOfRectangle}`);
+
+console.log(`The area of rectangle with base of ${base} and height of ${height} is ${areaOfRectangle}`);
