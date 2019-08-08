09dd1cef696999de6e69c9bb8086596b34d64287
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-08 18:09:32 -0500

    added Exercise 36

---
+++ js/index36.js
@@ -0,0 +1,8 @@
+let firstNumber = 10;
+let secondNumber = 20;
+
+let sameValue = firstNumber === secondNumber && secondNumber === firstNumber;
+
+let diffentValue = firstNumber != secondNumber && secondNumber != firstNumber;
+
+console.log(sameValue, diffentValue);
