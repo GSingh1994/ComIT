81d625b3aef9a1b3eec445e22e994611440dab2d
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-08 19:56:58 -0500

    added Exercise 39

---
+++ js/index39.js
@@ -0,0 +1,7 @@
+let firstName = "Gagandeep";
+let lastName = "Singh";
+
+console.log(`My first name is ${firstName} and it's ${firstName.length} characters long. \n
+My last name is ${lastName} and it's ${lastName.length} characters long \n
+The characters different between my first and last name is ${firstName.length - lastName.length} \n
+My first name is longer than my last name: ${firstName.length > lastName.length}`)
