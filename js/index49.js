7dae083fb4564a8939e49a941f903d4305c2e15e
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-10 16:52:17 -0500

    added Exercise 49

---
+++ js/index49.js
@@ -0,0 +1,7 @@
+let firstNumber;
+let secondNumber;
+firstNumber = 10;
+secondNumber = 5;
+if (firstNumber > secondNumber) {
+    console.log(`Number ${firstNumber} is bigger than ${secondNumber}`);
+}
