ccd6be0e8f05e1a9bafb7028e5a552dcae950734
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-10 15:30:02 -0500

    added Exercise 44

---
+++ js/index44.js
@@ -0,0 +1,9 @@
+let text = "HELLO";
+let result;
+
+result = text.charAt(4) + text.charAt(3) + text.charAt(2) + text.charAt(1) + text.charAt(0);
+
+result = result.toLowerCase();
+console.log(result);
+
+console.log(`The final result is : ${result}`)
