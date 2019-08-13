02b9e3cfc881a6217107c7beda58f47815fe68b9
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-13 14:49:08 -0500

    added Exercise 55

---
+++ js/index55.js
@@ -0,0 +1,15 @@
+let firstNumber;
+let secondNumber;
+firstNumber = 10;
+secondNumber = 5;
+
+(firstNumber > secondNumber) ? console.log(`Number ${firstNumber} is bigger than ${secondNumber}`) : console.log(`Number ${secondNumber} is bigger than ${firstNumber}`);
+
+
+
+
+// if (firstNumber > secondNumber) {
+//     console.log(`Number ${firstNumber} is bigger than ${secondNumber}`);
+// } else {
+//     console.log(`Number ${secondNumber} is bigger than ${firstNumber}`);
+// }
