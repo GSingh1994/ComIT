3378d5f93d3728a8a4b829798b465cca7e1750c3
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-13 14:51:16 -0500

    added Exercise 56

---
+++ js/index56.js
@@ -0,0 +1,14 @@
+let number = 10;
+
+(number % 2 === 0) ? console.log(`${number} is an even number`) : console.log(`${number} is an odd number`);
+
+
+
+
+
+
+// if (number % 2 === 0) {
+//     console.log(`${number} is an even number`)
+// } else {
+//     console.log(`${number} is an odd number`)
+// }
