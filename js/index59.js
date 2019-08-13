fd11818578f7a125887235b18ae63215ffea1128
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-13 15:05:01 -0500

    added Exercise 60

---
+++ js/index59.js
@@ -0,0 +1,12 @@
+let firstNumber, secondNumber;
+
+
+firstNumber = 24;
+secondNumber = 35;
+if (firstNumber === secondNumber) {
+    console.log("They are the same numbers")
+} else if (firstNumber > secondNumber) {
+    console.log(`${firstNumber} is bigger than ${secondNumber}`)
+} else if (secondNumber > firstNumber) {
+    console.log(`${secondNumber} is bigger than ${firstNumber}`)
+};
