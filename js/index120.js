c160a6c9ef5e7800920286a27413a565daaadcb3
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-18 13:51:48 -0500

    added Exercise 120

---
+++ js/index120.js
@@ -0,0 +1,12 @@
+let students = [];
+
+students[0] = 'Sam';
+students[1] = 'Diana';
+students[2] = 'thomas';
+students[3] = 'Aladipo';
+students[4] = 'Brenda';
+students[5] = 'Gloria';
+
+function classStudents(index) {
+    console.log('Index ' + index + ': ' + students[index])
+};
