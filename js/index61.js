7167363bcdcc8426cd94329886141e2398c1f6ad
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-18 14:47:52 -0500

    added Exercise 63

---
+++ js/index61.js
@@ -0,0 +1,48 @@
+// let weekdayNumber = 5;
+// // let message=console.log(`Today is $`)
+
+// if (weekdayNumber === 1) {
+//     console.log(`Today is Monday`)
+// } else if (weekdayNumber === 2) {
+//     console.log(`Today is Tuesday`)
+// } else if (weekdayNumber === 3) {
+//     console.log(`Today is Wednesday`)
+// } else if (weekdayNumber === 4) {
+//     console.log(`Today is Thursday`)
+// } else if (weekdayNumber === 5) {
+//     console.log(`Today is Friday`)
+// } else if (weekdayNumber === 6) {
+//     console.log(`Today is Saturday`)
+// } else if (weekdayNumber === 7) {
+//     console.log(`Today is Sunday`)
+// } else {
+//     console.log(`Error: Please input a number bettwen 1 and 7`)
+// }
+
+// SWITCH
+switch (weekdayNumber) {
+    case 1:
+        console.log(`Today is Monday`);
+        break;
+    case 2:
+        console.log(`Today is Tuesday`);
+        break;
+    case 3:
+        console.log(`Today is Monday`);
+        break;
+    case 4:
+        console.log(`Today is Monday`);
+        break;
+    case 5:
+        console.log(`Today is Monday`);
+        break;
+    case 6:
+        console.log(`Today is Monday`);
+        break;
+    case 7:
+        console.log(`Today is Monday`);
+        break;
+    default:
+        console.log(`Error: Please input a number bettwen 1 and 7`)
+
+}
---
+++ js/index63.js
@@ -0,0 +1,57 @@
+let year = 2009;
+let champion;
+
+// let result = `${champion} was the ${year} NHL Champion`
+
+switch (year) {
+    case 2006:
+        champion = 'Carolina Hurricanes';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2007:
+        champion = 'Anaheim Ducks';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2008:
+        champion = 'Detroit Red Wings';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2009:
+        champion = 'Pittsburgh Penguinas';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2010:
+        champion = 'Chicago Blackhawks';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2011:
+        champion = 'Boston Bruins';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2012:
+        champion = 'Los Angeles kings';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2013:
+        champion = 'Chicago Blackhawks';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2014:
+        champion = 'Los Angeles Kings';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2015:
+        champion = 'Chicago Blackhawks';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2016:
+        champion = 'Pittsburgh Penguins';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    case 2017:
+        champion = 'Pittsburgh Penguins';
+        console.log(`${champion} was the ${year} NHL Champion`);
+        break;
+    default:
+        console.log(`Please enter year between 2006 and 2017`);
+}
