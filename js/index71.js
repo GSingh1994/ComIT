f87eee24c0ece002254130ec18e0b6cf65d53c84
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-18 17:25:05 -0500

    added Exercise 71

---
+++ js/index71.js
@@ -0,0 +1,90 @@
+let timeTable = 1;
+currentTable = 1;
+currentNumber = 1
+
+console.log(`Time-table of one`)
+while (timeTable <= 10) {
+    currentTable = 1;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable++;
+}
+
+console.log(`Time-table of two`)
+timeTable = 2;
+while (timeTable <= 20) {
+    currentTable = 2;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable += 2;
+}
+
+console.log(`Time-table of three`)
+timeTable = 3;
+currentNumber = 1;
+while (timeTable <= 30) {
+    currentTable = 3;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable += 3;
+}
+
+console.log(`Time-table of four`)
+timeTable = 4;
+currentNumber = 1;
+while (timeTable <= 40) {
+    currentTable = 4;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable += 4;
+}
+
+console.log(`Time-table of five`)
+timeTable = 5;
+currentNumber = 1;
+while (timeTable <= 50) {
+    currentTable = 5;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable += 5;
+}
+
+console.log(`Time-table of six`)
+timeTable = 6;
+currentNumber = 1;
+while (timeTable <= 60) {
+    currentTable = 6;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable += 6;
+}
+
+console.log(`Time-table of seven`)
+timeTable = 7;
+currentNumber = 1;
+while (timeTable <= 70) {
+    currentTable = 7;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable += 7;
+}
+
+console.log(`Time-table of eight`)
+timeTable = 8;
+currentNumber = 1;
+while (timeTable <= 80) {
+    currentTable = 8;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable += 8;
+}
+
+console.log(`Time-table of nine`)
+timeTable = 9;
+currentNumber = 1;
+while (timeTable <= 90) {
+    currentTable = 9;
+    console.log(`${currentTable} times ${currentNumber} equals ${timeTable}`)
+    currentNumber++;
+    timeTable += 9;
+}
