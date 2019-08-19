18b0c50d038adaf2a0c6d571ce450d7dc2fd993e
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-18 21:50:58 -0500

    added Exercise 103

---
+++ js/index103.js
@@ -0,0 +1,14 @@
+let evenNumber = function () {
+    for (i = 0; i <= 100; i++) {
+        if (i % 2 === 0) {
+            console.log(i)
+        }
+    }
+};
+
+
+evenNumber();
+evenNumber();
+evenNumber();
+evenNumber();
+evenNumber();
