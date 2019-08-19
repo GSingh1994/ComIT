6f86a2b989e7bcf133e1f8c403bf98dce3441b37
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-18 21:53:06 -0500

    added Exercise 104

---
+++ js/index104.js
@@ -0,0 +1,13 @@
+let fibonacci = function () {
+    let f = 0;
+    let f1 = -1;
+    let f2 = 1;
+    for (n = 15; f < n; n++) {
+        f = f1 + f2;
+        f1 = f2;
+        f2 = f;
+        console.log(f)
+    }
+};
+
+fibonacci();
