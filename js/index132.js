7ce3974420c61e55b80da6ec1b5f76afb4b3304c
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-21 01:13:11 -0500

    added Exercise 132

---
+++ js/index132.js
@@ -0,0 +1,9 @@
+let mutants = ['Professor X', 'cyclops', 'Iceman', 'Angel', 'Beast', 'Phoenix', 'Logan'];
+
+if (mutants.indexOf('Logan') !== -1 && mutants.indexOf('Professor X') !== -1) {
+    console.log('We love X-men')
+};
+
+if (mutants.indexOf('Gambit') === -1) {
+    console.log('X-men sucks')
+};
