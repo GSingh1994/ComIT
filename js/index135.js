6cb20ec4ea1266de2891af50dd45c6c5850dba83
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-21 01:41:34 -0500

    added Exercise 135

---
+++ js/index135.js
@@ -0,0 +1,6 @@
+let mutants = ['Professor X', 'cyclops', 'Iceman', 'Angel', 'Magneto', 'Beast', 'Phoenix', 'Logan', 'Gambit'];
+count = 1;
+mutants.forEach(function (name) {
+    console.log(count + name)
+    count++
+});
