cd3110da645f036e3987f93385c8cbde91671a73
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-20 23:43:05 -0500

    added Exercise 129

--- js/index128.js
+++ js/index128.js
@@ -1,10 +1,11 @@
 let iceCreamFlavors = ['Chocolate', 'coffee', 'butter', 'vanilla', 'strawberry', 'peacan', 'cookie dough', 'mint', 'caramel', 'carrot cake'];
 
 
-iceCreamFlavors.pop();
-iceCreamFlavors.pop();
-iceCreamFlavors.pop();
-iceCreamFlavors.pop();
+console.log(iceCreamFlavors.pop());
+console.log(iceCreamFlavors.pop());
+console.log(iceCreamFlavors.pop());
+console.log(iceCreamFlavors.pop());
+
 
 
 console.log(iceCreamFlavors);
---
+++ js/index129.js
@@ -0,0 +1,5 @@
+let iceCreamFlavors = ['Chocolate', 'coffee', 'butter', 'vanilla', 'strawberry', 'peacan', 'cookie dough', 'mint', 'caramel', 'carrot cake'];
+
+for (i = 0; i < iceCreamFlavors.length; i++) {
+    console.log(iceCreamFlavors[i])
+};
