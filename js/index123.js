00e8e45c1619ccfc7804ca86ee1eee848f060cae
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-20 23:10:17 -0500

    added Exercise 123

---
+++ js/index123.js
@@ -0,0 +1,5 @@
+let students = ['Sam', 'Thomas'.toUpperCase(), 'Brenda', 'Gagan', 'Diana', 'Aladipo'.toUpperCase()];
+
+console.log(students);
+
+console.log(`The students array has ${students.length} items`);
