b43b763e3ef4a090234ee93aa952bdf78727b919
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-20 23:40:30 -0500

    added Exercise 128

---
+++ js/index128.js
@@ -0,0 +1,10 @@
+let iceCreamFlavors = ['Chocolate', 'coffee', 'butter', 'vanilla', 'strawberry', 'peacan', 'cookie dough', 'mint', 'caramel', 'carrot cake'];
+
+
+iceCreamFlavors.pop();
+iceCreamFlavors.pop();
+iceCreamFlavors.pop();
+iceCreamFlavors.pop();
+
+
+console.log(iceCreamFlavors);
