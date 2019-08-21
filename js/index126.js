c2099dabbf547f12e4fcaa8e269bf85238446076
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-20 23:21:14 -0500

    added Exercise 126

---
+++ js/index126.js
@@ -0,0 +1,14 @@
+let iceCreamFlavors = [];
+
+iceCreamFlavors.unshift('Chocolate');
+iceCreamFlavors.unshift('coffee');
+iceCreamFlavors.unshift('peanut-butter');
+iceCreamFlavors.unshift('vanilla');
+iceCreamFlavors.unshift('strawberry');
+iceCreamFlavors.unshift('peacan');
+iceCreamFlavors.unshift('cookie dough');
+iceCreamFlavors.unshift('mint');
+iceCreamFlavors.unshift('caramel');
+iceCreamFlavors.unshift('carrot cake');
+
+console.log(iceCreamFlavors);
