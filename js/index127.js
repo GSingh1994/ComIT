abce2ddb6a6397af21897b4c24b4afc9d20277da
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-20 23:38:20 -0500

    added Exercise 127

---
+++ js/index127.js
@@ -0,0 +1,8 @@
+let iceCreamFlavors = ['Chocolate', 'coffee', 'butter', 'vanilla', 'strawberry', 'peacan', 'cookie dough', 'mint', 'caramel', 'carrot cake'];
+
+// console.log(iceCreamFlavors);
+
+let firstFlavor = iceCreamFlavors.shift();
+let secondFlavor = iceCreamFlavors.shift();
+
+console.log(iceCreamFlavors);
