fd38bdafee633e6a0cff41f7ff92253024d4ef07
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-20 23:15:01 -0500

    added Exercise 124

---
+++ js/index124.js
@@ -0,0 +1,3 @@
+let movies = ['matrix', 'John wick', 'Titanic', 'Django', 'Pulp fiction', 'Kill bill', 'Reservior dogs', 'avengers', 'terminator', 'Safe-haven'];
+
+console.log(movies[0], movies[movies.length - 1]);
