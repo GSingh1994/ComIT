3f0bdfb72d67b472058d697d21dbed78b092c0e5
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-21 12:49:04 -0500

    added Exercise 144

---
+++ js/index144.js
@@ -0,0 +1,9 @@
+let user = {
+    username: 'batman',
+    password: 'Alfred1960ROCKS!'
+};
+
+console.log(user.username);
+console.log(user.password);
+
+// console.log((user.username).toUppercase());
