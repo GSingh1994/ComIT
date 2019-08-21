6245b719771f0ae23c4c5d2cef6153ebe36ffa35
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-20 23:19:50 -0500

    added Exercise 125

---
+++ js/index125.js
@@ -0,0 +1,14 @@
+let iceCreamFlavors = [];
+
+iceCreamFlavors.push('Chocolate');
+iceCreamFlavors.push('coffee');
+iceCreamFlavors.push('peanut-butter');
+iceCreamFlavors.push('vanilla');
+iceCreamFlavors.push('strawberry');
+iceCreamFlavors.push('peacan');
+iceCreamFlavors.push('cookie dough');
+iceCreamFlavors.push('mint');
+iceCreamFlavors.push('caramel');
+iceCreamFlavors.push('carrot cake');
+
+console.log(iceCreamFlavors);
