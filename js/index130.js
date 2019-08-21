3f043ffc53ba9e16d3eee8f4842db72c3b6dd63a
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-21 00:55:20 -0500

    added Exercise 130

---
+++ js/index130.js
@@ -0,0 +1,10 @@
+let mutants = ['Professor X', 'cyclops', 'Iceman', 'Angel', 'Beast', 'Phoenix'];
+
+let sortMutants = mutants.sort();
+let reverseMutants = mutants.reverse();
+let joinMutants = reverseMutants.join('*');
+
+// console.log(mutants);
+console.log(sortMutants);
+console.log(reverseMutants);
+console.log(joinMutants);
