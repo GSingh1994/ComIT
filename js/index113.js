7eb5525be0f67796ce3cf347dbac0a7c669fe733
Gagandeep_singh <singh.gagandeep0034@gmail.com>
2019-08-22 17:59:48 -0500

    made correction in ex_113

--- js/index113.js
+++ js/index113.js
@@ -2,9 +2,15 @@ function getShapePerimeter(base, height) {
     if (base === height) {
         perimeter = 4 * base;
     } else if (base !== height) {
-        perimeter = 2(base + height)
+        perimeter = 2 * (base + height)
     };
-    return perimeter;
-}
+    return `${perimeter} is the shape perimeter`;
+};
 
-console.log(getShapePerimeter(8, 8));
+console.log(getShapePerimeter(232, 8));
+
+if (perimeter > 100) {
+    console.log(`Perimeter is too big`)
+} else {
+    console.log(`The perimeter is fine`)
+};
